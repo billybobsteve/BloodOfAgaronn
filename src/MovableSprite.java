b0d4401fbb4a38d8c3c96f83a9931dccc1017eff
Chase Goddard <chase@chasegoddard.com>
2014-05-06 12:52:13 -0400

    Movable Sprite Changes

--- src/MovableSprite.java
+++ src/MovableSprite.java
@@ -2,13 +2,13 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 
 public class MovableSprite extends Sprite{
-	int xVelocity, yVelocity;
+	int xVelocity, yVelocity, ticks;
 	public MovableSprite(int x, int y, int width, int height, String fileName){
 		super(x,y,width,height,fileName);
 	}
 	
 	public void jump(){
-		
+		yVelocity=3;
 	}
 	
 	public void setXVelocity(int xvel){
@@ -17,6 +17,10 @@ public class MovableSprite extends Sprite{
 	
 	public void draw(Graphics g, ArrayList<Sprite> sprites){
 		x+=xVelocity;
+		y+=yVelocity;
+		if(yVelocity!=0){
+			
+		}
 		super.draw(g, sprites);
 	}
 }
