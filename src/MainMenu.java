f5355cda8df8f5872e42b75416b2616121931924
Thomas <Work@Thomass-MacBook-Air.local>
2014-05-08 09:18:56 -0400

    Initial commit

---
+++ src/MainMenu.java
@@ -0,0 +1,22 @@
+import java.awt.Graphics;
+
+
+public class MainMenu extends Screen {
+
+	public MainMenu() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Screen nextScreen() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
