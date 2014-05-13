19c80868a8f83d0ac4f2daadc6bfebd670b11a1a
Idoncar <nathanspring0@gmail.com>
2014-05-13 12:35:58 -0400

    Map Stuff

--- src/Map.java
+++ src/Map.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 
 
 public class Map {
-	Player player;
-	Room startingRoom;
+	static Player player;
+	static Room startingRoom;
 	public Map(Player p, Room original){
 		player = p;
 		startingRoom = original;
 	}
 
-	public Room generateMap(Room original, int n){
+	public static Room generateMap(Room original, int n){
 		if(n<10)
 			return original;
 		else if(original.doors.get(1).linkingRoom == null){
@@ -25,7 +25,7 @@ public class Map {
 		}
 	}
 	
-	public void generateNextLevel(Room r){
+	public static void generateNextLevel(Room r){
 		for(Door d : r.doors){
 			Room f = new Room(null, player, r,null);
 		}
@@ -36,7 +36,10 @@ public class Map {
 		Room original = new Room(null, null, null, null);
 		doors.add(new Door(1,1,1,1, null,original));
 		doors.add(new Door(1,1,1,1, null,original));
+		original.setDoors(doors);
 		Map map = new Map(null, original);
+		generateMap(startingRoom, 0);
+		
 		
 	}
 	
