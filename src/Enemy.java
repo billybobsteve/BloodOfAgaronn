a6adcddee92fa4ba6c773424408b35d1cb89cb7c
Chase Goddard <chase@chasegoddard.com>
2014-05-14 08:44:45 -0400

    EnemyControl added

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,9 +1,18 @@
 
 public class Enemy extends MovableSprite {
 	protected int damage;
-	public Enemy(int x, int y, int width, int height, String fileName, int damage) {
-		super(x, y, width, height, fileName);
+	protected boolean activated = false;
+	public Enemy(int x, int y, int width, int height, int health, String fileName, int damage) {
+		super(x, y, width, height, health, fileName);
 		this.damage = damage;
+	}
+	
+	public boolean isActive(){
+		return activated;
+	}
+	
+	public void activate(){
+		activated = true;
 	}
 	
 	public int getDamage(){
---
+++ src/EnemyControl.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class EnemyControl {
+	private ArrayList<Enemy> list;
+	private Player player;
+	private int activationDistance, enemySpeed;
+	public EnemyControl(ArrayList<Enemy> list, Player player, int activationDistance, int enemySpeed){
+		this.list = list;
+		this.player = player;
+		this.activationDistance = activationDistance;
+		this.enemySpeed = enemySpeed;
+	}
+	public void moveAll(){
+		for(Enemy e : list){
+			if(distance(player, e)<=activationDistance)
+				e.activate();
+			if(e.isActive()){
+				int dir = (player.getX()-e.getX())/Math.abs(player.getX()-e.getX());
+				e.setXVelocity(enemySpeed * dir);
+			}
+			if((Math.random())<.0001){
+				e.jump();
+			}
+		}
+	}
+	private int distance(MovableSprite m1, MovableSprite m2){
+		return (int)Math.sqrt(Math.pow(m1.getX()-m2.getX(),2)+Math.pow(m1.getY()-m2.getY(),2));
+	}
+}
