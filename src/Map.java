c22ee470adc54e206a37830d7d69bc427a29049f
Idoncar <nathanspring0@gmail.com>
2014-05-15 12:50:25 -0400

    Map Works (kind of)

--- src/Map.java
+++ src/Map.java
@@ -4,33 +4,26 @@ import java.util.ArrayList;
 public class Map {
 	static Player player;
 	static Room startingRoom;
+	static int counter = 0;
 	public Map(Player p, Room original){
 		player = p;
 		startingRoom = original;
 	}
 
 	public static Room generateMap(Room original, int n){
-		if(n<10)
+		if(n>10)
 			return original;
-		else if(original.doors.get(1).linkingRoom == null){
-			generateNextLevel(original.doors.get(1).linkingRoom);
-			return generateMap(original.doors.get(1).linkingRoom, n+1);
-		}
-		else if(original.doors.get(2).linkingRoom == null){
-			generateNextLevel(original.doors.get(2).linkingRoom);
-			return generateMap(original.doors.get(2).linkingRoom, n+1);
-		}
-		else{
-			return null;
-		}
+		generateNextLevel(original);
+		return generateMap(original, n+1);
 	}
-	
+
 	public static void generateNextLevel(Room r){
 		for(Door d : r.doors){
 			Room f = new Room(null, player, r,null);
+			counter++;
 		}
 	}
-	
+
 	public static void main(String[] args){
 		ArrayList<Door> doors = new ArrayList<Door>();
 		Room original = new Room(null, null, null, null);
@@ -39,10 +32,10 @@ public class Map {
 		original.setDoors(doors);
 		Map map = new Map(null, original);
 		generateMap(startingRoom, 0);
-		
-		
+		System.out.println(counter);
+
 	}
-	
-	
-	
+
+
+
 }
