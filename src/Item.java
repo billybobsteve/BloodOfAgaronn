5d478dbe8be4990f555f452f9bbdd2af1ba6800b
Chase Goddard <chase@chasegoddard.com>
2014-05-17 15:40:32 -0400

    Make items move with parent

--- src/Item.java
+++ src/Item.java
@@ -1,14 +1,20 @@
+import java.awt.Graphics;
+import java.util.ArrayList;
 
 public abstract class Item extends Sprite{
 	protected Sprite parent;
-	public Item(int x, int y, int width, int height, String fileName,Sprite parent) {
+	public Item(int x, int y, int width, int height, String fileName) {
 		super(x, y, width, height, fileName);
-		this.parent = parent;
 	}	
 	public Sprite parent(){
 		return parent;
 	}
 	public void setParent(Sprite s){
 		parent = s;
+	}
+	public void draw(Graphics g, ArrayList<Sprite> sprites){
+		this.x = parent.getX();
+		this.y = parent.getY();
+		super.draw(g, sprites);
 	}
 }
