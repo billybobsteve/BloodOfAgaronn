962360e4c5b73176d3de8fa8699f4c32e96ad65f
Thomas <Work@thomass-air.home>
2014-06-02 21:05:49 -0400

    Removed useless methods

--- src/Screen.java
+++ src/Screen.java
@@ -1,7 +1,5 @@
 import java.awt.Graphics;
-import java.util.ArrayList;
 
-import javax.swing.JComponent;
 
 public abstract class Screen{
 	public Screen(){
@@ -11,9 +9,4 @@ public abstract class Screen{
 	public abstract void draw(Graphics g);
 	public abstract Screen nextScreen();
 	
-	protected ArrayList<JComponent> components = new ArrayList<JComponent>();
-	
-	public ArrayList<JComponent> getJComponentsToDraw() {
-		return components;
-	}
 }
