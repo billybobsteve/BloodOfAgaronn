5900667a899c30dc2636bda871493975c3e94b12
Idoncar <nathanspring0@gmail.com>
2014-06-03 18:00:35 -0400

    Intelligent Platformness

--- src/RoomGenerator.java
+++ src/RoomGenerator.java
@@ -15,29 +15,35 @@ public class RoomGenerator {
 			if(d.getY() < sm.getHeight()-(Map.DOOR_HEIGHT + Map.FLOOR_HEIGHT))
 				sprites.add(new Sprite(d.getX()-50,d.getY()+Map.DOOR_HEIGHT,350,50,"BasedCutman.png"));
 		}
-		*/
+		 */
 		sprites.add(new Sprite(sm.getWidth(),sm.getHeight(),Map.WALL_WIDTH, Map.WALL_HEIGHT,null));
 		sprites.add(new Sprite(0,sm.getHeight(),Map.WALL_WIDTH, Map.WALL_HEIGHT,null));
+		int x = (int)(Math.random()*200);
+		for(int i = 1; i<(int)(Math.random()*5)+4; i++){
+			sprites.add(new Sprite(i*(x+200),(int)(Math.random()*300)+300,200,50,"BasedCutman.png"));
+		}
+		//sprites.add(new Sprite(500,425,200,50,"BasedCutman.png"));
+		//sprites.add(new Sprite(200,625,200,50,"BasedCutman.png"));
+		sprites.add(new Sprite((sm.getWidth()-Map.DOOR_WIDTH-Map.FLOOR_WIDTH)+100,200,350,50,"BasedCutman.png"));
 
-		
 
 		sprites.add(player.getWeapon());
 		sprites.add(player.getArmor());
 		sprites.add(player);
-		
+		/*
 		for(Door door : doors){
 			sprites.add(new Sprite(door.getX(),door.getY()+door.getHeight(),door.getWidth(),Map.FLOOR_HEIGHT,"BasedCutman.png"));
 			sprites.add(new Sprite(door.getX()-50-Map.FLOOR_WIDTH,door.getY()+door.getHeight()+Map.ENEMY_HEIGHT+20,Map.FLOOR_WIDTH,Map.FLOOR_HEIGHT,"BasedCutman.png"));
 		}
-		
+		 */
 		for(int i = 0;i<sm.getWidth();i+=256){
 			sprites.add(new Sprite(i,sm.getHeight()-Map.FLOOR_HEIGHT,Map.FLOOR_WIDTH,Map.FLOOR_HEIGHT,floorName));
-			
+
 		}
 		Room r = new Room(doors, player, previous, sprites, ec, sm);
 		return r;
 	}
-	
+
 	public static Room getStartingRoom(ArrayList<Door> doors, Player player, Room previous, ScreenManager sm){
 		ArrayList<Enemy> enemies = generateStartingEnemies(sm);
 		EnemyControl ec = new EnemyControl(enemies, player, sm.getFractionOfScreenX(.2), sm.getFractionOfScreenX(.01));
@@ -57,7 +63,7 @@ public class RoomGenerator {
 		Room r = new Room(doors, player, previous, sprites, ec, sm);
 		return r;
 	}
-	
+
 	public static ArrayList<Enemy> generateEnemies(int n, ScreenManager manager){
 		ArrayList<Enemy> enemies = new ArrayList<Enemy>();
 		for(int i = 0; i<n; i++){
